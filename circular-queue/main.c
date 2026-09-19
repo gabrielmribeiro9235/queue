cc5368f73a359de08d879a59d89de53d8c087667
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-19 16:46:27 -0300

    feat(circular-queue): create the menu function in the main file

--- circular-queue/main.c
+++ circular-queue/main.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 #include"queue.h"
 
+void menu() {
+    printf("-----------------------------------------------\n");
+    printf("Select a function:\n");
+    printf("1 - print_queue\n");
+    printf("2 - in\n");
+    printf("3 - out\n");
+    printf("4 - is_empy\n");
+    printf("5 - is_full\n");
+    printf("6 - size\n");
+    printf("7 - exit\n");
+    printf("-----------------------------------------------\n");
+    printf("Your choice: ");
+}
+
 int main() {
 
     return 0;
