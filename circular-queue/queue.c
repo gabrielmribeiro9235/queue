c1dfc35449288175ec706a982fec129e71394b82
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-19 16:22:42 -0300

    feat(circular-queue): create the in function

--- circular-queue/queue.c
+++ circular-queue/queue.c
@@ -47,3 +47,15 @@ int is_full(t_queue *queue) {
 int size(t_queue *queue) {
     return queue == NULL ? 0 : queue->size;
 }
+
+int in(t_queue *queue, int elem) {
+    if (is_full(queue)) {
+        return 0;
+    }
+
+    queue->end = (queue->end + 1) % queue->max;
+    queue->items[queue->end] = elem;
+    queue->size++;
+
+    return 1;
+}
