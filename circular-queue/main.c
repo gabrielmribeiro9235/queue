0920390e1ed490551cf24337ec7a38c22b03119a
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-19 16:50:51 -0300

    feat(circular-queue): create switch-case statement to the in function

--- circular-queue/main.c
+++ circular-queue/main.c
@@ -39,6 +39,24 @@ int main() {
                 printf("-----------------------------------------------\n");
                 print_queue(queue);
                 break;
+            case 2: {
+                printf("-----------------------------------------------\n");
+
+                int elem;
+
+                printf("Enter the element you want to insert: ");
+                scanf("%d", &elem);
+
+                int in_status = in(queue, elem);
+
+                if (in_status == 1) {
+                    printf("\n%d inserted successfully\n", elem);
+                } else {
+                    printf("\nFailed to insert\n");
+                }
+
+                break;
+            }
             default:
                 printf("-----------------------------------------------\n");
                 printf("Invalid choice\n");
